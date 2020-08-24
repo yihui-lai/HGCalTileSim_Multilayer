a86f889aff0f20d05558071ae5b30b2b2a17f3dc
Yihui Lai <yihui.lai@cern.ch>
2020-08-24 15:08:21 -0400

    update SubmitJobs.py

--- condor/SubmitJobs.py
+++ condor/SubmitJobs.py
@@ -17,29 +17,51 @@ parser.add_argument('--beamy',
                     nargs='+',
                     default=[0],
                     help='List of y values of beam center')
+parser.add_argument('--tile_number',
+                    '-n',
+                    type=int,
+                    default=3,
+                    help='Number of tile layers')
+parser.add_argument('--is_wrap_ESR',
+                    '-i',
+                    type=int,
+                    default=1,
+                    help='1 for use ESR, 0 for use Tyvek')
 parser.add_argument('--tilewidth',
                     '-L',
                     type=float,
                     nargs='+',
                     default=[30],
                     help='Tile width')
+parser.add_argument('--tilethick',
+                    '-t',
+                    type=float,
+                    nargs='+',
+                    default=[3],
+                    help='Tile thickness')
+parser.add_argument('--beamwidth',
+                    '-w',
+                    type=float,
+                    nargs='+',
+                    default=[1.5],
+                    help='width of beam [mm]')
 parser.add_argument('--dimplerad',
                     '-r',
                     type=float,
                     nargs='+',
-                    default=[3],
+                    default=[2.65],
                     help='List of dimple radius to try out')
 parser.add_argument('--dimpleind',
                     '-d',
                     type=float,
                     nargs='+',
-                    default=[1.5],
+                    default=[1.6],
                     help='List of dimple indents to try out')
 parser.add_argument('--sipmwidth',
                     '-W',
                     type=float,
                     nargs='+',
-                    default=[1.4],
+                    default=[1.3],
                     help='SiPM width paramter')
 parser.add_argument('--sipmrim',
                     '-R',
@@ -51,8 +73,14 @@ parser.add_argument('--sipmstand',
                     '-S',
                     type=float,
                     nargs='+',
-                    default=[0.3],
+                    default=[0.2],
                     help='SiPM stand height')
+parser.add_argument('--gap_pcb_wrap',
+                    '-G',
+                    type=float,
+                    nargs='+',
+                    default=[0.2],
+                    help='Gap between PCB and wrap')
 parser.add_argument('--absmult',
                     '-a',
                     type=float,
@@ -60,7 +88,7 @@ parser.add_argument('--absmult',
                     default=[1.],
                     help='List of tile absorption length multiple')
 parser.add_argument('--wrapreflect',
-                    '-w',
+                    '-m',
                     type=float,
                     nargs='+',
                     default=[0.985],
@@ -75,38 +103,46 @@ parser.add_argument('--pcbradius',
                     '-b',
                     type=float,
                     nargs='+',
-                    default=[2.3],
+                    default=[3.175],
                     help='List of pcb exposed radii to test')
 parser.add_argument('--NEvents',
                     '-N',
                     type=int,
                     default=100,
                     help='Number of events to run')
-parser.add_argument('--tile_number',
-                    '-n',
-                    type=int,
-                    default=3,
-                    help='Number of tile layers')
-parser.add_argument('--is_wrap_ESR',
-                    '-i',
+parser.add_argument('--Material',
+                    '-E',
                     type=int,
                     default=1,
-                    help='1 for use ESR, 0 for use Tyvek')
+                    help='Material, 1EJ, 2SCSN')
+parser.add_argument('--dimplesa',
+                    '-D',
+                    type=float,
+                    nargs='+',
+                    default=[1.3],
+                    help='dimple Sigma Alpha')
 parser.add_argument('--prefix',
                     type=str,
                     default='',
                     help='Additional string to place in filename')
-
 args = parser.parse_args()
 
 BASE_DIR = os.path.abspath(os.environ['CMSSW_BASE'] + '/src/' +
                            '/HGCalTileSim/condor/')
-DATA_DIR = os.path.abspath(BASE_DIR + '/results/')
-
+DATA_DIR = os.path.abspath(BASE_DIR + '/sphereDimple_abs/')
+#r510-0-4.privnet, 5, 6, 9, 10, 11
+#siab-1.umd.edu
+#r540-0-20.privnet, 21
+#720-0-2.privnet, 1
+#
 CONDOR_JDL_TEMPLATE = """
 universe              = vanilla
 Executable            = {0}/condor-LYSquareTrigger_CMSSW.sh
 should_transfer_files = NO
+Requirements = TARGET.Machine =?= "r510-0-4.privnet" || TARGET.Machine =?= "r510-0-5.privnet" || TARGET.Machine =?= "r510-0-6.privnet"|| TARGET.Machine =?= "r510-0-9.privnet"|| TARGET.Machine =?= "r510-0-10.privnet"|| TARGET.Machine =?= "r510-0-11.privnet" || TARGET.Machine =?= "r540-0-20.privnet" || TARGET.Machine =?= "r540-0-21.privnet" || TARGET.Machine =?= "r720-0-2.privnet" || TARGET.Machine =?= "r720-0-1.privnet" || TARGET.Machine =?= "siab-1.umd.edu"
+#Requirements = TARGET.Machine =?= "r540-0-20.privnet" || TARGET.Machine =?= "r540-0-21.privnet"
+#Requirements =  TARGET.Machine =?= "r720-0-2.privnet" || TARGET.Machine =?= "r720-0-1.privnet"
+#Requirements =  TARGET.Machine =?= "siab-1.umd.edu"
 Requirements          = TARGET.FileSystemDomain == "privnet"
 request_memory        = 1 GB
 Output                = {1}.stdout
@@ -116,27 +152,29 @@ Arguments             = {2}
 Queue
 """
 
-for x, y, L, r, d, a, w, W, p, b, R, S in [(x, y, L, r, d, a, w, W, p, b, R, S)
+for x, y, L, t, w, r, d, W, R, S, G, a, m, p, b, D in [(x, y, L, t, w, r, d, W, R, S, G, a, m, p, b, D)
                                            for x in args.beamx
                                            for y in args.beamy
                                            for L in args.tilewidth
+                                           for t in args.tilethick
+                                           for w in args.beamwidth
                                            for r in args.dimplerad
                                            for d in args.dimpleind
-                                           for a in args.absmult
-                                           for w in args.wrapreflect
                                            for W in args.sipmwidth
+                                           for R in args.sipmrim
+                                           for S in args.sipmstand
+                                           for G in args.gap_pcb_wrap
+                                           for a in args.absmult
+                                           for m in args.wrapreflect
                                            for p in args.pcbreflect
                                            for b in args.pcbradius
-                                           for R in args.sipmrim
-                                           for S in args.sipmstand]:
+                                           for D in args.dimplesa]:
 
   def make_str(prefix):
     args_string = '_'.join([
-        'x{0:.1f}'.format(x), 'y{0:.1f}'.format(y), 'L{0:.1f}'.format(L),
-        'r{0:.1f}'.format(r), 'd{0:.1f}'.format(d), 'a{0:.1f}'.format(
-            a * 100), 'm{0:.1f}'.format(
-                w * 100), 'W{0:.1f}'.format(W), 'P{:.1f}'.format(p * 100),
-        'Pr{:.1f}'.format(b), 'R{0:.1f}'.format(R), 'S{0:.2f}'.format(S), 'n{}'.format(args.tile_number), 'i{}'.format(args.is_wrap_ESR),
+        'x{0:.1f}'.format(x), 'y{0:.1f}'.format(y), 'n{}'.format(args.tile_number), 'esr_{}'.format(args.is_wrap_ESR), 'L{0:.1f}'.format(L), 'thick_{0:.1f}'.format(t),
+        'beamwidth_{0:.1f}'.format(w), 'r{0:.1f}'.format(r), 'd{0:.1f}'.format(d), 'sipmW_{0:.1f}'.format(W), 'R{0:.1f}'.format(R), 'S{0:.2f}'.format(S*100),'G{0:.1f}'.format(G*100),
+        'a{0:.1f}'.format(a*100), 'm{0:.1f}'.format(m*1000), 'P{:.1f}'.format(p*100),'Pcbr_{:.1f}'.format(b), 'DimpleSA_{:.1f}'.format(D),
     ])
     return prefix + args.prefix + '_' + args_string.replace('.', 'p')
 
@@ -144,12 +182,13 @@ for x, y, L, r, d, a, w, W, p, b, R, S in [(x, y, L, r, d, a, w, W, p, b, R, S)
                                   make_str('hgcal_tilesim') + '.root')
 
   condor_args = ' '.join([
-      '-x {}'.format(x), '-y {}'.format(y), '-L {}'.format(L), '-w 1.5',
-      '-r {}'.format(r), '-d {}'.format(d), '-a {}'.format(a), '-m {}'.format(w),
-      '-W {}'.format(W), '-p {}'.format(p), '-b {}'.format(b), '-R {}'.format(R),
-      '-S {}'.format(S), '-N {}'.format(args.NEvents), '-n {}'.format(args.tile_number),
-       '-i {}'.format(args.is_wrap_ESR), '-o {}'.format(
-          os.path.abspath(save_filename)),
+      '-x {}'.format(x), '-y {}'.format(y), '-n {}'.format(args.tile_number),'-i {}'.format(args.is_wrap_ESR),
+      '-L {}'.format(L),'-t {}'.format(t), '-w {}'.format(w),
+      '-r {}'.format(r), '-d {}'.format(d), '-W {}'.format(W),
+      '-R {}'.format(R), '-S {}'.format(S),'-G {}'.format(G),
+      '-a {}'.format(a), '-m {}'.format(m),
+      '-p {}'.format(p), '-b {}'.format(b),'-D {}'.format(D), '-N {}'.format(args.NEvents), '-E {}'.format(args.Material),
+      '-o {}'.format(os.path.abspath(save_filename)),
   ])
 
   log_filename = os.path.abspath(DATA_DIR + '/condor/' + '/' +
